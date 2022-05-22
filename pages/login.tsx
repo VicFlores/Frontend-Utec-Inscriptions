ad24d6d056d1fca071d14603e27a2c44b91d0df0
Vic Flores <vicsito2014@gmail.com>
2022-05-22 09:14:21 -0600

    Login page was added

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/Layout/Layout';
+
+const login = () => {
+  return (
+    <div>
+      <Layout>
+        <h1>You're login</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default login;
