290dbca2875e32a33b747944d29a63fb6e07cb57
Vic Flores <vicsito2014@gmail.com>
2022-05-21 23:55:03 -0600

    feat: Stylization header was implemented

---
+++ components/Header/Header.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { ThemeProvider } from 'styled-components';
+import { AiOutlineMenu } from 'react-icons/ai';
+import {
+  AppName,
+  AppNameContainer,
+  HeaderButton,
+  HeaderContainer,
+  LinkRouter,
+  NavContainer,
+  theme,
+  invertTheme,
+  BurgerMenu,
+  BurgerMenuContainer,
+} from './styles';
+
+const Header = () => {
+  const [menu, setMenu] = useState<boolean>(false);
+
+  const handleMenu = () => {
+    setMenu(!menu);
+  };
+
+  return (
+    <HeaderContainer>
+      <AppNameContainer>
+        <AppName>Utec-inscriptions</AppName>
+      </AppNameContainer>
+
+      <BurgerMenuContainer>
+        <BurgerMenu onClick={handleMenu}>
+          <AiOutlineMenu />
+        </BurgerMenu>
+      </BurgerMenuContainer>
+
+      <NavContainer hidden={menu}>
+        <ThemeProvider theme={theme}>
+          <Link href="/" passHref>
+            <LinkRouter>Home</LinkRouter>
+          </Link>
+          <Link href="/" passHref>
+            <LinkRouter>Portal</LinkRouter>
+          </Link>
+          <Link href="/" passHref>
+            <LinkRouter>Classroom</LinkRouter>
+          </Link>
+
+          <HeaderButton>
+            <Link href="/" passHref>
+              <LinkRouter>SignUp</LinkRouter>
+            </Link>
+          </HeaderButton>
+          <ThemeProvider theme={invertTheme}>
+            <HeaderButton>
+              <Link href="/" passHref>
+                <LinkRouter>Login</LinkRouter>
+              </Link>
+            </HeaderButton>
+          </ThemeProvider>
+        </ThemeProvider>
+      </NavContainer>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
