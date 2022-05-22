fc2ed829fce9988fa57da6ea4e5e52f6bdcd83af
Vic Flores <vicsito2014@gmail.com>
2022-05-22 16:57:20 -0600

    feat: Login page was added

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Layout from '../components/Layout/Layout';
+import LoginForm from '../components/LoginForm/LoginForm';
 
-const login = () => {
+function login() {
   return (
     <div>
       <Layout>
-        <h1>You're login</h1>
+        <LoginForm
+          src="https://res.cloudinary.com/vicflores11/image/upload/v1653237006/undraw_secure_login_pdn4_xfisbg.png"
+          cols="repeat(2, 400px)"
+          textButton="Join"
+          height="90vh"
+        />
       </Layout>
     </div>
   );
-};
+}
 
 export default login;
