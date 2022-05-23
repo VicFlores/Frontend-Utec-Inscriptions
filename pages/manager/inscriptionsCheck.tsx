62ee7116b51e607f3504bb3d3c41fa880c0d5a81
Vic Flores <vicsito2014@gmail.com>
2022-05-23 15:37:54 -0600

    Incription page was implemented

---
+++ pages/manager/inscriptionsCheck.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import InscriptionsTable from '../../components/inscriptionsTable/inscriptionsTable';
+import Layout from '../../components/Layout/Layout';
+
+const InscriptionsCheck = () => {
+  return (
+    <Layout>
+      <InscriptionsTable />
+    </Layout>
+  );
+};
+
+export default InscriptionsCheck;
