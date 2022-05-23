2ab32004f4baa3fcaf369d7b4c4ab8f438377fcd
Vic Flores <vicsito2014@gmail.com>
2022-05-22 23:02:04 -0600

    feat: props was added

--- pages/inscriptions.tsx
+++ pages/inscriptions.tsx
@@ -8,9 +8,15 @@ function Inscriptions() {
       <Layout>
         <InscriptionsForm
           src="https://res.cloudinary.com/vicflores11/image/upload/v1653255681/undraw_text_field_htlv_ok6ufw.png"
-          cols="400px"
+          cols="626px"
           textButton="Inscribe"
           height="0"
+          mt="60px"
+          imgWidth="500px"
+          formCol="repeat(2, 1fr)"
+          formsHeight="500px"
+          btnColStart="1"
+          btnColEnd="3"
         />
       </Layout>
     </div>
