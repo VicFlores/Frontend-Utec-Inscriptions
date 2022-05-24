23db763e0d4b9e81361abf2ea9f8fb4388ea3666
Vic Flores <vicsito2014@gmail.com>
2022-05-24 11:25:26 -0600

    feat: middleware was added

---
+++ pages/student/_middleware.ts
@@ -0,0 +1,18 @@
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+import { verifyAuth } from '../../utils/verifyAuth';
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const url = req.nextUrl.clone();
+  url.pathname = '/';
+  const token = req.cookies['token'];
+
+  if (!token) {
+    return NextResponse.rewrite(url);
+  }
+
+  const result = await verifyAuth(token);
+  if (result.message) {
+    return NextResponse.rewrite(url);
+  }
+  return NextResponse.next();
+}
